01a284437e9bcd088d0d35c1696691a82340897a
THEAbhishekjoshi <abhishekjoshi.739.ak@gmail.com>
2026-05-28 18:29:30 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,70 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-export default function Home() {
+export default function MainPage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className=" h-full">
+
+      <div className='m-3 rounded-lg'>
+        <section className="py-20 bg-[url('/valley1.jpg')] bg-cover bg-bottom rounded-t-lg">
+          <div className="flex items-center justify-center p-5 ">
+            <div className="px-7 py-2 md:px-20 rounded-full gap-7 text-black 
+            flex items-center 
+            bg-white/70 backdrop-opacity-20">
+              {/* bg-gray-300/40 backdrop-blur-md */}
+              <button>Home</button>
+              <button>Work</button>
+              <button>Projects</button>
+            </div>
+
+          </div>
+        </section>
+
+        <div className="relative py-20 bg-[url('/valley1.jpg')] bg-cover bg-bottom  rounded-b-lg">
+          <div
+            className="
+            absolute
+            left-0
+            right-0
+            top-0
+            h-full
+            bg-black/20
+            backdrop-blur-xl
+            rounded-b-lg
+            border border-white/10
+
+            px-10 py-8
+          "
+          >
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, minima officia recusandae aut, reprehenderit consequatur aperiam dolore pariatur quam voluptatum illum! Odit sit, eos rerum quaerat molestiae nostrum explicabo libero.</p>
+          </div>
+
+
+        </div>
+      </div>
+
+
+      {/* <div className="flex items-center justify-center p-5 ">
+        <div className="py-2 px-20  rounded-full gap-7 text-black bg-black-50
+        flex items-center shadow-md
+        ">`
+          <button>Home</button>
+          <button>Work</button>
+          <button>Projects</button>
+        </div>
+
+      </div>
+
+      <div className="bg-pink-300 m-4 rounded-lg h-150">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src='/image.png'
+          width={500}
+          height={100}
+          alt='profile image'
+          className='object-cover object-bottom w-full h-full rounded-lg'
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div> */}
+
+
     </div>
-  );
+  )
 }
